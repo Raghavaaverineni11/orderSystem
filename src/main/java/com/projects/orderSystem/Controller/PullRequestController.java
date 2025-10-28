0f861125a60d1c101237462c60bb04ca7ba40c88
Raghavaaverineni11 <raghavaoffc@gmail.com>
2025-10-27 23:40:05 -0500

    ListAll orders implemented

--- src/main/java/com/projects/orderSystem/Controller/PullRequestController.java
+++ src/main/java/com/projects/orderSystem/Controller/PullRequestController.java
@@ -1,8 +1,16 @@
 package com.projects.orderSystem.Controller;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PullRequestController {
+
+    @GetMapping("/ListAllOrders")
+    public ResponseEntity<?> getAllOrder(){
+        return ResponseEntity.ok("String");
+    }
+
 }
