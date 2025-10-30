d8b22861729dd1ca859e20ff227b4f1634b7813c
Raghavaaverineni11 <raghavaoffc@gmail.com>
2025-10-29 23:50:41 -0500

    headers,response entity,body - added for pushrequest controller

--- src/main/java/com/projects/orderSystem/Controller/PullRequestController.java
+++ src/main/java/com/projects/orderSystem/Controller/PullRequestController.java
@@ -1,16 +1,24 @@
 package com.projects.orderSystem.Controller;
 
 
+import com.projects.orderSystem.Service.Booking;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PullRequestController {
 
+    @Autowired
+    Booking bookingservice;
+
     @GetMapping("/ListAllOrders")
     public ResponseEntity<?> getAllOrder(){
-        return ResponseEntity.ok("String");
+        List<String> name=bookingservice.getNames();
+        return ResponseEntity.ok().header("application","Json").body(name);
     }
 
 }
