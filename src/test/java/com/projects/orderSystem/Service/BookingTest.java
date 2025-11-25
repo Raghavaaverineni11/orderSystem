2f727d489333928d99fb863ed27f9be932d5d895
Raghavaaverineni11 <raghavaoffc@gmail.com>
2025-11-24 21:26:02 -0600

    test classes 2

--- src/test/java/com/projects/orderSystem/Service/BookingTest.java
+++ src/test/java/com/projects/orderSystem/Service/BookingTest.java
@@ -2,10 +2,20 @@ package com.projects.orderSystem.Service;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 public class BookingTest {
+
+    @Autowired
+    private Booking Booking;
+
+
+
+
+
 
 
 }
