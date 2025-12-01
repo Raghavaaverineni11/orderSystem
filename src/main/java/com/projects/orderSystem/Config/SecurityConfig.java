8ba69bdf7310d64b9af3e35b5dc6bcce63351cd6
Raghavaaverineni11 <raghavaoffc@gmail.com>
2025-11-30 22:32:09 -0600

    http bAsic Auth with In memory Database added

--- src/main/java/com/projects/orderSystem/Config/SecurityConfig.java
+++ src/main/java/com/projects/orderSystem/Config/SecurityConfig.java
@@ -4,6 +4,10 @@ package com.projects.orderSystem.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,7 +18,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
-                .formLogin(withDefaults());
+                .httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("pass")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
