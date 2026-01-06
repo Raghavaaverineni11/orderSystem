a103b75eca4f122c37570e0a992b4f8599689691
Raghavaaverineni11 <raghavaoffc@gmail.com>
2026-01-05 23:09:38 -0600

    .

--- src/main/java/com/projects/orderSystem/Service/Booking.java
+++ src/main/java/com/projects/orderSystem/Service/Booking.java
@@ -15,6 +15,7 @@ public class Booking {
 
     List<String> names= Arrays.asList("raghava","rakesh","Ajay","vadina","nani","ashwitha");
     List<String> orderSrtatus =Arrays.asList("Delivered","IN the Way","Shipped","Dispatched");
+    List<String> DSA=Arrays.asList("two pointer","BinarySearch","bruteForce");
 
 
 
