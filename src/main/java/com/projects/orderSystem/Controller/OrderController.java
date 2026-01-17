0c2280ffaac6544a49d217b9b721beb38cf2c23c
Raghavaaverineni11 <raghavaoffc@gmail.com>
2026-01-17 12:41:36 -0600

    List All orders Api creaated

--- src/main/java/com/projects/orderSystem/Controller/OrderController.java
+++ src/main/java/com/projects/orderSystem/Controller/OrderController.java
@@ -1,4 +1,21 @@
 package com.projects.orderSystem.Controller;
 
+
+import com.projects.orderSystem.Entity.Users;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class OrderController {
+
+
+    @GetMapping("AllOrders")
+    public ResponseEntity<List<Users>>  getAllOrders(){
+
+
+
+    }
 }
